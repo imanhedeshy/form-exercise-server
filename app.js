014fb0927c9aaad643a964267da0b03cd36deb5d
Iman Hedeshy <iman.hedeshy@gmail.com>
2024-03-08 02:30:48 -0500

    add express server

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const fsPromises = require("fs").promises;
+
+const cors = require("cors");
+app.use(cors());
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use(express.json());
+
+const { PORT, CORS_ORIGIN_WHITELIST } = process.env || {
+  PORT: 8080,
+  CORS_ORIGIN_WHITELIST: "http://localhost:3000",
+};
+
+app.route("/").get((req, res) => {
+  console.log(req.body, req.headers, req.query, req.params);
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
